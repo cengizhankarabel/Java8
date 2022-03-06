0a4cf8031d561121f22b8468725fdd9a148689d8
CENGIZHAN KARABEL <69279953+cengizhankarabel@users.noreply.github.com>
2022-03-05 22:00:16 -0600

    "BiPredicate example using with BiConsumer"

--- src/com/java8/features/predicate/ConsumerPredicateExample.java
+++ src/com/java8/features/predicate/ConsumerPredicateExample.java
@@ -5,6 +5,7 @@ import com.java8.features.repository.PersonRepository;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -14,10 +15,13 @@ public class ConsumerPredicateExample {
 
     static Predicate<Person> predicate2 = person -> person.getGender().equals("Male");
 
+    static BiPredicate<Integer, String> biPredicate = (height, gender) -> height >=140 && gender.equals("Male");
+
     static BiConsumer<String, List<String>> hobbiesConsumer = (name,hobbies)-> System.out.println(name+" "+hobbies);
 
     static Consumer<Person> personConsumer = person -> {
-        if(predicate1.and(predicate2).test(person)){
+      //  if(predicate1.and(predicate2).test(person)){
+        if(biPredicate.test(person.getHeight(),person.getGender())){
             hobbiesConsumer.accept(person.getName(), person.getHobbies());
         }
     };
