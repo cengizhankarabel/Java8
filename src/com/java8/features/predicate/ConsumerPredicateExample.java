be4ed1ccc3640ca5759b528af8a5ecb58f515818
CENGIZHAN KARABEL <69279953+cengizhankarabel@users.noreply.github.com>
2022-03-05 21:52:28 -0600

    "Consumer, BiConsumer, Predicate using together"

---
+++ src/com/java8/features/predicate/ConsumerPredicateExample.java
@@ -0,0 +1,29 @@
+package com.java8.features.predicate;
+
+import com.java8.features.entity.Person;
+import com.java8.features.repository.PersonRepository;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class ConsumerPredicateExample {
+
+    static Predicate<Person> predicate1 = person -> person.getHeight() >=140;
+
+    static Predicate<Person> predicate2 = person -> person.getGender().equals("Male");
+
+    static BiConsumer<String, List<String>> hobbiesConsumer = (name,hobbies)-> System.out.println(name+" "+hobbies);
+
+    static Consumer<Person> personConsumer = person -> {
+        if(predicate1.and(predicate2).test(person)){
+            hobbiesConsumer.accept(person.getName(), person.getHobbies());
+        }
+    };
+
+    public static void main(String[] args) {
+        List<Person> personList = PersonRepository.getAllPerson();
+        personList.forEach(personConsumer);
+    }
+}
